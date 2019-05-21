33bd8086b57a467fb189d7585c9dbb0de857f233
Syaifudin <saifudin@dikanetwork.com>
2019-05-21 12:35:21 +0700

    skaj

--- resources/views/front/album.blade.php
+++ resources/views/front/album.blade.php
@@ -5,7 +5,7 @@
 @section('meta-keywords', $album->album)
 @section('meta-news_keywords', $album->album)
 @section('meta-deskripsi', $album->album)
-@section('meta-image', env('APP_URL').$album->foto()->first()->foto)
+@section('meta-image', '//cdn.example.com/uploads/images/webpage_300x200.png')
 
 @section('css')
 <link rel="stylesheet" href="{{asset('vendor/galeri/css/lc_lightbox.css')}}">
