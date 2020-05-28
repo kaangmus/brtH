87028212975d49f43479cf102a58dfd83ecd3e5e
unknown <syaifudin.ama@gmail.com>
2020-05-29 05:32:44 +0700

    as

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,1 +1,22 @@
-<img src="{{asset('images/maintenance.png')}}" alt="" srcset="" style="width: 100%; max-width: 400px;">
+<style>
+    .container {
+      height: 200px;
+      position: relative;
+      border: 3px solid green;
+    }
+    
+    .vertical-center {
+      margin: 0;
+      position: absolute;
+      top: 50%;
+      -ms-transform: translateY(-50%);
+      transform: translateY(-50%);
+    }
+    </style>
+
+<div class="container">
+    <div class="vertical-center">
+    <img src="{{asset('images/maintenance.jpg')}}" alt="" style="width: 100%; max-width: 800px;">
+    </div>
+</div>
+
